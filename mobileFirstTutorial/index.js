2b209a5975eb7453c7e3f878288bc3b3a228e35b
Austin Michael Regan <austinmregan@gmail.com>
2019-09-06 12:22:37 -0400

    added spin animation

--- mobileFirstTutorial/index.js
+++ mobileFirstTutorial/index.js
@@ -5,7 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './Apps/App';
 import App2 from './Apps/App2';
-import App3 from './Apps/App3';
+import App3 from './Apps/App3/App3';
 
 import {name as appName} from './app.json';
 
