a412873c325935931363fb4b97349b4329f2fdef
Austin Michael Regan <austinmregan@gmail.com>
2019-09-06 14:12:42 -0400

    removed routing from files

--- mobileFirstTutorial/index.js
+++ mobileFirstTutorial/index.js
@@ -6,7 +6,6 @@ import {AppRegistry} from 'react-native';
 import App from './Apps/App';
 import App2 from './Apps/App2';
 import App3 from './Apps/App3';
-import App4 from './Apps/App4/App4';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
@@ -14,5 +13,3 @@ AppRegistry.registerComponent(appName, () => App);
 // AppRegistry.registerComponent(appName, () => App2);
 
 // AppRegistry.registerComponent(appName, () => App3);
-
-// AppRegistry.registerComponent(appName, () => App4);
