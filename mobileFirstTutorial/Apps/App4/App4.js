2ffb1b1c40aeccb97e0d434cdcc7be70fcee9ec0
Austin Michael Regan <austinmregan@gmail.com>
2019-09-06 13:19:19 -0400

    started navigation app file

---
+++ mobileFirstTutorial/Apps/App4/App4.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {StyleSheet, Text, View, AppRegistry} from 'react-native';
+
+import {createStackNavigator, createAppContainer} from 'react-navigation';
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
+  render() {
+    const {navigate} = this.props.navigation;
+    return (
+      <Button
+        title="Go to Jane's profile"
+        onPress={() => navigate('Profile', {name: 'Jane'})}
+      />
+    );
+  }
+}
+
+const MainNavigator = createStackNavigator({
+  Home: {screen: HomeScreen},
+  Profile: {screen: ProfileScreen},
+});
+
+const App = createAppContainer(MainNavigator);
+
+export default App;
--- mobileFirstTutorial/index.js
+++ mobileFirstTutorial/index.js
@@ -9,10 +9,10 @@ import App3 from './Apps/App3';
 import App4 from './Apps/App4/App4';
 import {name as appName} from './app.json';
 
-// AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => App);
 
 // AppRegistry.registerComponent(appName, () => App2);
 
 // AppRegistry.registerComponent(appName, () => App3);
 
-AppRegistry.registerComponent(appName, () => App4);
+// AppRegistry.registerComponent(appName, () => App4);
